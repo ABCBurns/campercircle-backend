dc3a1059562ab3c63bbd6a382ffd125c6eb7c310
Christoph Perick <christoph.perick@gmail.com>
2025-09-27 19:12:52 +0200

    comments

--- backend/app/database.py
+++ backend/app/database.py
@@ -13,14 +13,17 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# -------------------------
+# -----------------------------------------------------------------------------
 # Create tables
-# -------------------------
+# -----------------------------------------------------------------------------
 Base.metadata.create_all(bind=engine)
 
-# -------------------------
+# -----------------------------------------------------------------------------
 # Conditionally create GIST index
-# -------------------------
+# -----------------------------------------------------------------------------
+# Check if index idx_users_locations exists if not then create it.
+# It creates a GIST index on User.location in the users table.
+# GIST index ensures ST_DWithin and ST_Distance queries scale.
 with engine.begin() as conn:
     conn.execute(
         text("""
